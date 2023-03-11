c9183e6d7a2245572d28b1da563c32f83756b457
Kozou <louisfloquey@gmail.com>
2023-03-11 23:21:48 +0100

    Create Nuxt 3 project with eslint, prettier and TailwindCSS

---
+++ nuxt.config.ts
@@ -0,0 +1,14 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  build: {
+    postcss: {
+      postcssOptions: {
+        plugins: {
+          tailwindcss: {},
+          autoprefixer: {},
+        },
+      },
+    },
+  },
+  css: ['@/assets/css/main.css'],
+});
